b971529d9f695d0f6382e019a169534f5abb744c
Juno AM <sabotaione@gmail.com>
2021-02-18 20:17:29 -0500

    added week 3 content

--- Assets/Scripts/GridMovement.cs
+++ Assets/Scripts/GridMovement.cs
@@ -6,12 +6,16 @@ public class GridMovement : MonoBehaviour
 {
     //new code!!!!
 	//we assigned these to be equivalent in each direction, so fwd = (x = 0, y = 0, z = 2) and bwd = (x = 0, y = 0, z = -2)
-	public Vector3 fwd;
+	public Vector3 fwd, bwd, lft, rgt, up, dwn;
+    public Transform hazard, key;
+    Vector3 startPos;
+    public bool hasKey;
 
     // Start is called before the first frame update
     void Start() //like setup
     {
-        
+        startPos = transform.position;
+        hasKey = false;
     }
 
     // Update is called once per frame
@@ -21,15 +25,35 @@ public class GridMovement : MonoBehaviour
     	//access the Input class method called GetKeyDown, which takes a KeyCode parameter
     	//This will return true when this specific key is pressed down and false when it isn't
         if (Input.GetKeyDown(KeyCode.W)) { 
-
         	
         	//this is similar to print or println in processing
         	Debug.Log("you pressed w :)");
 
-
         	//modify the position of the transform of whatever gameobject this is attached to
         	//offset it by fwd (move from the current position to the current position plus the value of fwd)
         	transform.position += fwd; 
+        }
+        if (Input.GetKeyDown(KeyCode.S)) { 
+           transform.position += bwd; 
+        }
+        if (Input.GetKeyDown(KeyCode.A)) { 
+            transform.position += lft; 
+        }
+        if (Input.GetKeyDown(KeyCode.D)) {            
+            transform.position += rgt; 
+        }
+        if (Input.GetKeyDown(KeyCode.Q)) { 
+            transform.position += dwn; 
+        }
+        if (Input.GetKeyDown(KeyCode.E)) { 
+            transform.position += up; 
+        }
+
+        if (hazard.position == transform.position) {
+            transform.position = startPos;
+        }
+        if (key.position == transform.position) {
+            hasKey = true;
         }
 
     }
