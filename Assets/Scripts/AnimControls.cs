ec6bedf0736bbc1184de9496cbd5447456c1614f
Juno AM <sabotaione@gmail.com>
2021-04-08 20:46:10 -0400

    added week 9 review content

--- Assets/Scripts/AnimControls.cs
+++ Assets/Scripts/AnimControls.cs
@@ -16,8 +16,20 @@ public class AnimControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    	if (Input.GetKey(KeyCode.W)){ //if the key w is being pressed, then set the parameter value to 1
-        	akai.SetInteger("AnimState",1); //Animator.SetInteger will assign a value to the parameter indicated to the value 
-    	}
+        akai.SetInteger("AnimState", 0);
+        if (Input.GetKey(KeyCode.W)){ //if the key w is being pressed, then set the parameter value to 1
+            akai.SetInteger("AnimState",1); //Animator.SetInteger will assign a value to the parameter indicated to the value 
+
+            if (Input.GetKey(KeyCode.LeftShift)){
+                akai.SetInteger("AnimState", 2);
+                Debug.Log("should be working");
+            }
+        }
+
+        /*
+        if (Input.GetKey(KeyCode.C)){ //if the key w is being pressed, then set the parameter value to 1
+            akai.SetInteger("AnimState",2); //Animator.SetInteger will assign a value to the parameter indicated to the value 
+        }
+        */
     }
 }
